59ea6c774c6b709852ac8554f983f0624660d466
unknown <bruno.oliveira@redspark.io>
2021-05-19 20:14:00 -0300

    versao 1.4.17

--- src/main/kotlin/br/com/meumetro/service/impl/AppServiceImpl.kt
+++ src/main/kotlin/br/com/meumetro/service/impl/AppServiceImpl.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service
 @Service
 class AppServiceImpl : AppService {
 
-    private val versionApp = "1.4.16"
+    private val versionApp = "1.4.17"
 
     override fun isLatestVersion(versionApp: String): Boolean {
         return this.versionApp == versionApp
