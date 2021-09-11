103ad0f4a68205d4108c1d3d7fd8d1d04d034235
Bruno Oliveira <bruno.oliveira@redspark.io>
2021-09-11 01:47:55 -0300

    v1.4.19

--- src/main/kotlin/br/com/meumetro/service/impl/AppServiceImpl.kt
+++ src/main/kotlin/br/com/meumetro/service/impl/AppServiceImpl.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service
 @Service
 class AppServiceImpl : AppService {
 
-    private val versionApp = "1.4.18"
+    private val versionApp = "1.4.19"
 
     override fun isLatestVersion(versionApp: String): Boolean {
         return this.versionApp == versionApp
